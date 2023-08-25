b3da44f769a1996dba99dbd372d7af4c89d3c4e9
gym <3521834081@qq.com>
2023-08-25 17:27:10 +0800

    show line

--- ruoyi-ui/src/views/Student/ExerciseAchievement/index.vue
+++ ruoyi-ui/src/views/Student/ExerciseAchievement/index.vue
@@ -1,48 +1,213 @@
 <template>
   <div>
     {{ title }}
-    <ul v-for="indexA in time "  :key="indexA" class="ul-box">
-        <template  v-for="(item , index ) in splitData[indexA-1]" >
-        <show-box  :isShow="true" :key="index" :content="item.content"> </show-box>
-      </template>
+
+    <ul v-for="indexA in time" :key="indexA" class="ul-box">
+      <div
+        class="content-box"
+        :style="indexA % 2 === 0 ? { flexDirection: 'row-reverse' } : {}"
+      >
+        <template v-for="(item, index) in splitData[indexA - 1]">
+          <show-box
+            :key="index"
+            :isShow="
+              indexA % 2 === 0
+                ? index !== 0
+                : index + 1 !== splitData[indexA - 1].length
+            "
+            :content="item.content"
+            :name="item.name"
+            :question="item.question"
+            :option="item.option"
+            :type="item.type"
+            :answer="item.answer"
+            :img="item.img"
+          >
+          </show-box>
+        </template>
+      </div>
+      <div
+        class="border-box"
+        v-if="indexA !== time"
+        :style="indexA % 2 !== 0 ? { width: '85%' } : { width: '10%' }"
+      ></div>
     </ul>
-
-
-
   </div>
 </template>
 
 <script>
+import fontJson from "./a.json";
 import ShowBox from "@/views/Student/ExerciseAchievement/showBox.vue";
-
+import axios from "axios";
 export default {
   name: "ExerciseAchievement",
-  components: {ShowBox},
+  components: { ShowBox },
 
   data() {
     return {
       title: "成绩",
-      dataList:[],
-      splitData:{},
-      time:0
-    }
+      dataList: [],
+      splitData: {},
+      time: 0,
+      show: false,
+    };
   },
+  computed: {},
   created() {
-    this.dataList = [ {id:0,content:'333'},{id:1,content:'222'},{id:2,content:'123'},{id:2,content:'<img src="./bad.png"><img>'} ]
-    this.time = Math.ceil( this.dataList.length / 4 +1)
-    for (let i = 0; i < this.dataList.length; i += 4) {
-      const group = this.dataList.slice(i, i + 4);
-      this.splitData[i / 4] = group;
-    }
-    console.log(   0/4 )
-  }
+    this.getData();
+    /* 配置的字段 */
+    // 前端保存图片内容 根据id进行对应
+    /* 后端获取字段 */
+    // id 题目编号 字符串 有1-2类型
+    // name 按钮名称
+    // answer: 填空题存的是字符串数组json形式"["你好","你是谁"]"  选择题A选项  多选题"["A","B","C"]" 简答题直接是字符串
+    // 如果没有就是返回空字符串
+  },
+  methods: {
+    async getData() {
+      this.show = false;
+      try {
+        const trueData = await axios.get("", { 实验id: 1, userid: 1 });
+        // 经过转化
+        // const serverData =trueData.data
+        // 模拟数据
+        const serverData = [
+          {
+            id: "0",
+            name: "图片",
+          },
+          {
+            id: "0-1",
+            name: "第一个",
+            answer: '["12","13","14"]',
+          },
+          {
+            id: 1,
+            name: "第二个",
+            answer: "B",
+          },
+          {
+            id: 2,
+            name: "第三个",
+            answer: '["A","B"]',
+          },
+          {
+            id: 3,
+            name: "第四个",
+            answer: "简答答案sadfsadfsdfsdfds",
+          },
+        ];
+        this.dataList = this.mergedData(serverData);
+        this.time = Math.ceil(this.dataList.length / 4);
 
-}
+        for (let i = 0; i < this.dataList.length; i += 4) {
+          const group = this.dataList.slice(i, i + 4);
+          this.splitData[i / 4] = group;
+        }
+        this.show = true;
+      } catch (E) {
+        console.log(E);
+      }
+
+      // 下面是模拟
+      this.test();
+    },
+    test() {
+      const serverData = [
+        {
+          id: "0",
+          name: "图片",
+        },
+        {
+          id: "0-1",
+          name: "第一个",
+          answer: '["12","13","14"]',
+        },
+        {
+          id: 1,
+          name: "第二个",
+          answer: "B",
+        },
+        {
+          id: 2,
+          name: "第三个",
+          answer: '["A","B"]',
+        },
+        {
+          id: 3,
+          name: "第四个",
+          answer: "简答答案sadfsadfsdfsdfds",
+        },
+        {
+          id: "0",
+          name: "图片",
+        },
+        {
+          id: "0-1",
+          name: "第一个",
+          answer: '["12","13","14"]',
+        },
+        {
+          id: 1,
+          name: "第二个",
+          answer: "B",
+        },
+        {
+          id: 2,
+          name: "第三个",
+          answer: '["A","B"]',
+        },
+        {
+          id: 3,
+          name: "第四个",
+          answer: "简答答案sadfsadfsdfsdfds",
+        },
+      ];
+      this.dataList = this.mergedData(serverData);
+      this.time = Math.ceil(this.dataList.length / 4);
+
+      for (let i = 0; i < this.dataList.length; i += 4) {
+        const group = this.dataList.slice(i, i + 4);
+        this.splitData[i / 4] = group;
+      }
+      this.show = true;
+    },
+    // 合并数据
+    mergedData(serverData) {
+      // 创建一个映射，将 frontendData 转换成以 id 为键的对象
+      const frontendDataMap = fontJson.reduce((map, item) => {
+        map[item.id] = item;
+        return map;
+      }, {});
+
+      // 后端数据合并到前端
+      const mergedAndFilteredData = serverData.map((serverItem) => {
+        const frontendItem = frontendDataMap[serverItem.id];
+        return { ...frontendItem, ...serverItem };
+      });
+      return mergedAndFilteredData;
+    },
+  },
+};
 </script>
 
 <style scoped>
-.ul-box{
-  display: flex;
+.ul-box {
+  width: 95%;
+  padding: 0;
+  margin: 0;
+  margin-left: 5%;
+}
+
+.border-box {
+  height: 60px;
+  border-right: 1px solid #1b9fe8;
+  width: 85%;
+}
+
+.content-box {
   width: 100%;
+  display: flex;
+  flex-wrap: wrap;
 }
 </style>
--- ruoyi-ui/src/views/Student/ExerciseAchievement/showBox.vue
+++ ruoyi-ui/src/views/Student/ExerciseAchievement/showBox.vue
@@ -1,53 +1,182 @@
 <template>
-    <li class="li-box">
-      <el-popover
-        ref="popover"
-        placement="bottom"
-        title="标题"
-        width="200"
-        trigger="click"
-        <template   > <img src="./bad.png"/> </template>
-        <el-button class="button-box" slot="reference">click 激活</el-button>
-      </el-popover>
-      <div v-show="isShow" class="box-span"></div>
+  <li class="li-box">
+    <el-popover ref="popover" placement="bottom" trigger="click">
+      <el-button class="button-box" slot="reference"> {{ name }}</el-button>
+      <div class="content-box">
+        <!-- 展示图片 -->
+        <div v-if="!answer">
+          <img :src="img" class="img-box" />
+        </div>
 
-    </li>
+        <!-- 填空 -->
+        <div
+          class="fill"
+          v-if="type === 1"
+          v-html="`${wrapContent(question)}`"
+        ></div>
+        <div v-if="type === 2">
+          <div>{{ question }}</div>
+          <div class="answer-box">
+            <div
+              v-for="(item, index) in option"
+              :key="index"
+              class="answer-content"
+              :style="transAnswer == index ? { backgroundColor: '#fff' } : {}"
+            >
+              <span :style="transAnswer == index ? { color: '#40a9ff' } : {}">
+                {{ item }}</span
+              >
+            </div>
+          </div>
+        </div>
+        <div class="" v-if="type === 3">
+          <div>{{ question }}</div>
+          <div class="answer-box">
+            <div
+              v-for="(item, index) in option"
+              :key="index"
+              class="answer-content"
+              :style="
+                transAnswer.includes(index) ? { backgroundColor: '#fff' } : {}
+              "
+            >
+              <span
+                :style="transAnswer.includes(index) ? { color: '#40a9ff' } : {}"
+              >
+                {{ item }}</span
+              >
+            </div>
+          </div>
+        </div>
+        <div class="" v-if="type === 4">
+          <div>{{ question }}</div>
+          <div class="answer-box answer-color">
+            {{ answer }}
+          </div>
+        </div>
+      </div>
+      <!-- <div v-html="content" class="content-box"></div> -->
+    </el-popover>
+    <div v-if="isShow" class="box-span"></div>
+  </li>
 </template>
 
 <script>
 export default {
   name: "showBox",
-  props:{
+  props: {
     isShow: {
-      type:Boolean,
-      default:true
+      type: Boolean,
+      default: true,
     },
-    content:{
-
-    }
+    type: {},
+    content: {},
+    option: {},
+    name: {},
+    question: {},
+    answer: {},
+    img: {},
   },
   data() {
-    return {
+    return {};
+  },
+  computed: {
+    transAnswer() {
+      try {
+        //解析json
+        const answer = this.answer && JSON.parse(this.answer);
+        return this.mapCharacters(answer);
+      } catch (error) {
+        // 如果报错说明不用解析
+        return this.mapCharacters(this.answer);
+      }
+    },
+  },
+  methods: {
+    wrapContent(content) {
+      const dataArray = (this.answer && JSON.parse(this.answer)) || [];
+      console.log();
 
-    }
-  }
-}
+      let index = 0;
+      const replacedText = content.replace(
+        /\[文本框\]/g,
+        () => `<span class="deep-span"> ${dataArray[index++] || ""}</span>`
+      );
+      return replacedText;
+    },
+    mapCharacters(input) {
+      const mapping = {
+        A: 0,
+        B: 1,
+        C: 2,
+        D: 3,
+        E: 4,
+        F: 5,
+        G: 6,
+      };
+
+      if (typeof input === "string") {
+        // 如果输入是字符串
+        const result = [];
+        for (let i = 0; i < input.length; i++) {
+          const char = input[i];
+          if (mapping[char] !== undefined) {
+            result.push(mapping[char]);
+          }
+        }
+        return result;
+      } else if (Array.isArray(input)) {
+        // 如果输入是数组
+        return input.map((char) => mapping[char]);
+      } else {
+        return [];
+      }
+    },
+  },
+};
 </script>
 
 <style scoped>
-
-.li-box{
+.li-box {
   position: relative;
   width: 25%;
   list-style: none;
   padding: 0;
 }
 
-.button-box{
-  width: 80%;
+.img-box {
+  width: 100%;
+}
+.content-box {
+  width: 500px;
 }
 
-.box-span{
+.button-box {
+  width: 80%;
+  border-radius: 100px;
+  height: 40px;
+}
+
+.answer-content {
+  padding: 5px 10px 5px;
+  font-weight: bolder;
+}
+.answer-content:not(:nth-child(1)) {
+  margin-top: 10px;
+}
+
+.answer-box {
+  padding: 10px 8px;
+  margin-top: 10px;
+  background-color: #f8f8f8;
+}
+
+.answer-color {
+  color: #1b9fe8;
+  font-weight: bolder;
+}
+
+.box-span {
   height: 1px;
   border-top: 1px solid #1b9fe8;
   width: 20%;
@@ -55,5 +184,14 @@ export default {
   right: 0;
   transform: translateY(-50%);
   top: 50%;
+}
+</style>
+<style>
+.deep-span {
+  color: #40a9ff;
+  display: inline-block;
+  font-weight: bolder;
+  border-bottom: 1px solid #40a9ff;
+  padding: 0 15px;
 }
 </style>
